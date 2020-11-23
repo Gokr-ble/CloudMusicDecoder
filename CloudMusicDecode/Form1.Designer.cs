39a1c911fd73356779909ac12d60510ffbd159fe
Gokr-ble <luansiyuanhao@163.com>
2020-11-23 16:49:46 +0800

    first commit

---
+++ CloudMusicDecode/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace CloudMusicDecode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chooseInputPath = new System.Windows.Forms.Button();
+            this.inputPath = new System.Windows.Forms.TextBox();
+            this.inputPathLabel = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chooseOutPath = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.outPath = new System.Windows.Forms.TextBox();
+            this.outPathLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chooseInputPath);
+            this.groupBox1.Controls.Add(this.inputPath);
+            this.groupBox1.Controls.Add(this.inputPathLabel);
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(758, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "输入";
+            // 
+            // chooseInputPath
+            // 
+            this.chooseInputPath.Location = new System.Drawing.Point(709, 41);
+            this.chooseInputPath.Name = "chooseInputPath";
+            this.chooseInputPath.Size = new System.Drawing.Size(30, 27);
+            this.chooseInputPath.TabIndex = 2;
+            this.chooseInputPath.Text = "...";
+            this.chooseInputPath.UseVisualStyleBackColor = true;
+            this.chooseInputPath.Click += new System.EventHandler(this.chooseInputPath_Click);
+            // 
+            // inputPath
+            // 
+            this.inputPath.Location = new System.Drawing.Point(82, 41);
+            this.inputPath.Name = "inputPath";
+            this.inputPath.Size = new System.Drawing.Size(620, 27);
+            this.inputPath.TabIndex = 1;
+            // 
+            // inputPathLabel
+            // 
+            this.inputPathLabel.AutoSize = true;
+            this.inputPathLabel.Location = new System.Drawing.Point(6, 47);
+            this.inputPathLabel.Name = "inputPathLabel";
+            this.inputPathLabel.Size = new System.Drawing.Size(69, 20);
+            this.inputPathLabel.TabIndex = 0;
+            this.inputPathLabel.Text = "文件路径";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chooseOutPath);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.outPath);
+            this.groupBox2.Controls.Add(this.outPathLabel);
+            this.groupBox2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(758, 125);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "输出";
+            // 
+            // chooseOutPath
+            // 
+            this.chooseOutPath.Location = new System.Drawing.Point(709, 44);
+            this.chooseOutPath.Name = "chooseOutPath";
+            this.chooseOutPath.Size = new System.Drawing.Size(30, 27);
+            this.chooseOutPath.TabIndex = 3;
+            this.chooseOutPath.Text = "...";
+            this.chooseOutPath.UseVisualStyleBackColor = true;
+            this.chooseOutPath.Click += new System.EventHandler(this.chooseOutPath_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(82, 88);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(274, 24);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "在缓存文件目录下创建同名mp4文件";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // outPath
+            // 
+            this.outPath.Location = new System.Drawing.Point(82, 44);
+            this.outPath.Name = "outPath";
+            this.outPath.Size = new System.Drawing.Size(620, 27);
+            this.outPath.TabIndex = 2;
+            // 
+            // outPathLabel
+            // 
+            this.outPathLabel.AutoSize = true;
+            this.outPathLabel.Location = new System.Drawing.Point(6, 47);
+            this.outPathLabel.Name = "outPathLabel";
+            this.outPathLabel.Size = new System.Drawing.Size(69, 20);
+            this.outPathLabel.TabIndex = 1;
+            this.outPathLabel.Text = "输出路径";
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.startButton.Location = new System.Drawing.Point(12, 249);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(90, 30);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "开始转换";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.closeButton.Location = new System.Drawing.Point(680, 249);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(90, 30);
+            this.closeButton.TabIndex = 3;
+            this.closeButton.Text = "退出";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(109, 249);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(565, 29);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(782, 303);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "网易云缓存转换";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button chooseInputPath;
+        private System.Windows.Forms.TextBox inputPath;
+        private System.Windows.Forms.Label inputPathLabel;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button chooseOutPath;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox outPath;
+        private System.Windows.Forms.Label outPathLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ CloudMusicDecode/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CloudMusicDecode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chooseInputPath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = false;
+            fileDialog.Title = "选择文件";
+            fileDialog.Filter = "uc缓存文件(*.uc)|*.uc";
+            if(fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                inputPath.Text = fileDialog.FileName;
+            }
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void chooseOutPath_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog fileDialog = new SaveFileDialog();
+            fileDialog.Title = "选择保存路径";
+            fileDialog.Filter = "音频文件(*.mp4)|*.mp4";
+            if(fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                outPath.Text = fileDialog.FileName;
+            }
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == true)
+            {
+                outPath.Enabled = false;
+                if(inputPath.Text != "")
+                {
+                    int length = inputPath.Text.Length;
+                    string str = inputPath.Text.Substring(0, length-3);
+                    outPath.Text = str + ".mp4";
+                }      
+            }
+            else
+            {
+                outPath.Enabled = true;
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            
+
+            if (inputPath.Text == "" || outPath.Text == "")
+            {
+                MessageBox.Show("请选择正确的文件路径！", "错误", MessageBoxButtons.OK);
+            }
+            else
+            {
+                FileStream inputFile = new FileStream(inputPath.Text, FileMode.Open);
+                FileStream outputFile = new FileStream(outPath.Text, FileMode.Create, FileAccess.Write);
+                byte[] buf = new byte[1024];
+
+                progressBar1.Visible = true;
+                progressBar1.Minimum = 1;
+                progressBar1.Maximum = (int)new FileInfo(inputPath.Text).Length;
+                progressBar1.Step = 1024;
+
+                while (true)
+                {
+                    int len = inputFile.Read(buf, 0, buf.Length);
+                    if(len > 0)
+                    {
+                        for (int i = 0; i < len; i++)
+                        {
+                            buf[i] ^= 0xa3;
+                        }
+                        outputFile.Write(buf, 0, len);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    startProgress();
+                }
+                outputFile.Close();
+                inputFile.Close();
+                
+            }
+        }
+
+        private void startProgress()
+        {
+            progressBar1.PerformStep();
+        }
+    }
+}
